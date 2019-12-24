a7d87f154a529f86644aca645c8ca6f2e6c0c0a6
Sonikflame <sonikflame@mail.ru>
2019-12-24 08:05:15 +0200

    Допилка модального окна

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+   /* Модальное окно */
    var button = $('#button');
    var modal = $('#modal');
    var close = $('#close');
@@ -11,6 +12,7 @@ $(document).ready(function(){
       modal.removeClass('modal_active');
    });
 
+   /* Слайдер */
    $('.slider').slick({
       infinite: true,
       slidesToShow: 3,
@@ -32,8 +34,38 @@ $(document).ready(function(){
              slidesToScroll: 1
            }
          }
-       ]
-    });
+      ]
+   });
 
-    new WOW().init();
+   /* Инициализация анимации */
+   new WOW().init();
+
+   /* Валидация формы */
+   $('#brif-form').validate({
+      rules: {
+         phone: {
+            required: true
+         }
+      }, 
+      messages: {
+         phone: {
+            required: "Укажите телефон"
+         }
+      }
+   });
+   $('#modal-form').validate({
+      rules: {
+         phone: {
+            required: true
+         }
+      }, 
+      messages: {
+         phone: {
+            required: "<br>Укажите телефон<br>"
+         }
+      }
+   });
+
+   /* Маска для телефона */
+   $('.phone').mask('+7 (999) 999-99-99');
 });
